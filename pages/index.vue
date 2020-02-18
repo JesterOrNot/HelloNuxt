da592662832154bcb0f49884680f4dc10c40aad0
Sean Hellum <seanhellum45@gmail.com>
2020-02-18 04:37:10 +0000

    Update messafge

--- pages/index.vue
+++ pages/index.vue
@@ -3,23 +3,11 @@
     <div>
       <logo />
       <h1 class="title">
-        HelloNuxt
+        Hello
       </h1>
       <h2 class="subtitle">
-        My prime Nuxt.js project
+        Hello World From Nuxt.js
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
@@ -60,9 +48,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
